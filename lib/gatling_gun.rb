aec548b353d6336b590d962b38d2edc0ca961fd1
Anil Rhemtulla <anil@platphormcorp.com>
2012-12-18 11:36:02 -0800

    Using ruby 1.87 hash syntax

--- lib/gatling_gun.rb
+++ lib/gatling_gun.rb
@@ -30,17 +30,17 @@ class GatlingGun
            details[:html] or details["html"]
       fail ArgumentError, "either text or html must be provided as a detail"
     end
-    make_api_call("add", details.merge(name: newsletter))
+    make_api_call("add", details.merge(:name => newsletter))
   end
 
   def edit_newsletter(newsletter, details)
     fail ArgumentError, "details must be a Hash"  unless details.is_a? Hash
     fail ArgumentError, "details cannot be empty" if     details.empty?
-    make_api_call("edit", details.merge(name: newsletter))
+    make_api_call("edit", details.merge(:name => newsletter))
   end
 
   def get_newsletter(newsletter)
-    make_api_call("get", name: newsletter)
+    make_api_call("get", :name => newsletter)
   end
 
   def list_newsletters(newsletter = nil)
@@ -50,7 +50,7 @@ class GatlingGun
   end
   
   def delete_newsletter(newsletter)
-    make_api_call("delete", name: newsletter)
+    make_api_call("delete", :name => newsletter)
   end
   
   #############
@@ -59,13 +59,13 @@ class GatlingGun
   
   def add_list(list, details = { })
     fail ArgumentError, "details must be a Hash" unless details.is_a? Hash
-    make_api_call("lists/add", details.merge(list: list))
+    make_api_call("lists/add", details.merge(:list => list))
   end
   
   def edit_list(list, details = { })
     fail ArgumentError, "details must be a Hash"  unless details.is_a? Hash
     fail ArgumentError, "details cannot be empty" if     details.empty?
-    make_api_call("lists/edit", details.merge(list: list))
+    make_api_call("lists/edit", details.merge(:list => list))
   end
   
   def get_list(list = nil)
@@ -76,7 +76,7 @@ class GatlingGun
   alias_method :list_lists, :get_list
   
   def delete_list(list)
-    make_api_call("lists/delete", list: list)
+    make_api_call("lists/delete", :list => list)
   end
   
   ##############
@@ -90,12 +90,12 @@ class GatlingGun
                 else            fail ArgumentError,
                                      "details must be a Hash or Array"
                 end
-    make_api_call("lists/email/add", list: list, data: json_data)
+    make_api_call("lists/email/add", :list => list, :data => json_data)
   end
   alias_method :add_emails, :add_email
   
   def get_email(list, emails = nil)
-    parameters         = {list: list}
+    parameters         = {:list => list}
     parameters[:email] = emails if emails
     make_api_call("lists/email/get", parameters)
   end
@@ -103,7 +103,7 @@ class GatlingGun
   alias_method :list_emails, :get_email
   
   def delete_email(list, emails)
-    make_api_call("lists/email/delete", list: list, email: emails)
+    make_api_call("lists/email/delete", :list => list, :email => emails)
   end
   alias_method :delete_emails, :delete_email
   
@@ -118,17 +118,17 @@ class GatlingGun
         fail ArgumentError, "#{field} is a required detail"
       end
     end
-    make_api_call("identity/add", details.merge(identity: identity))
+    make_api_call("identity/add", details.merge(:identity => identity))
   end
   
   def edit_identity(identity, details)
     fail ArgumentError, "details must be a Hash"  unless details.is_a? Hash
     fail ArgumentError, "details cannot be empty" if     details.empty?
-    make_api_call("identity/edit", details.merge(identity: identity))
+    make_api_call("identity/edit", details.merge(:identity => identity))
   end
   
   def get_identity(identity)
-    make_api_call("identity/get", identity: identity)
+    make_api_call("identity/get", :identity => identity)
   end
   
   def list_identities(identity = nil)
@@ -138,7 +138,7 @@ class GatlingGun
   end
   
   def delete_identity(identity)
-    make_api_call("identity/delete", identity: identity)
+    make_api_call("identity/delete", :identity => identity)
   end
   
   ##################
@@ -146,18 +146,18 @@ class GatlingGun
   ##################
   
   def add_recipient(newsletter, list)
-    make_api_call("recipients/add", name: newsletter, list: list)
+    make_api_call("recipients/add", :name => newsletter, :list => list)
   end
   alias_method :add_recipients, :add_recipient
 
   def get_recipient(newsletter)
-    make_api_call("recipients/get", name: newsletter)
+    make_api_call("recipients/get", :name => newsletter)
   end
   alias_method :get_recipients,  :get_recipient
   alias_method :list_recipients, :get_recipient
   
   def delete_recipient(newsletter, list)
-    make_api_call("recipients/delete", name: newsletter, list: list)
+    make_api_call("recipients/delete", :name => newsletter, :list => list)
   end
   alias_method :delete_recipients, :delete_recipient
   
@@ -166,22 +166,22 @@ class GatlingGun
   #################
   
   def add_schedule(newsletter, details = { })
-    parameters      = {after: details[:after]}
+    parameters      = {:after => details[:after]}
     parameters[:at] = details[:at].iso8601.sub("T", " ") \
       if details[:at].respond_to? :iso8601
     if not details[:after].nil? and ( not details[:after].is_a?(Integer) or 
                                       details[:after] < 1 )
       fail ArgumentError, "after must be a positive integer"
     end
-    make_api_call("schedule/add", parameters.merge(name: newsletter))
+    make_api_call("schedule/add", parameters.merge(:name =>  newsletter))
   end
   
   def get_schedule(newsletter)
-    make_api_call("schedule/get", name: newsletter)
+    make_api_call("schedule/get", :name => newsletter)
   end
   
   def delete_schedule(newsletter)
-    make_api_call("schedule/delete", name: newsletter)
+    make_api_call("schedule/delete", :name => newsletter)
   end
   
   #######
@@ -189,7 +189,7 @@ class GatlingGun
   #######
   
   def make_api_call(action, parameters = { })
-    ApiCall.new( action, parameters.merge( api_user: @api_user,
-                                           api_key:  @api_key ) ).response
+    ApiCall.new( action, parameters.merge( :api_user => @api_user,
+                                           :api_key =>  @api_key ) ).response
   end
 end
